57d0376a9d8aa877e5d515c3a3a126690fe28263
Shubham Gupta <shubham272101@gmail.com>
2024-10-27 19:49:08 +0530

    add segregate 0's and 1's problem from gfg

---
+++ Segregate0and1.java
@@ -0,0 +1,23 @@
+public class Segregate0and1 {
+    void segregate0and1(int[] arr) {
+        // code here
+
+        int l = 0;
+        int r = arr.length-1;
+        while(l<r){
+            while(arr[l] == 0 && l<r){
+                l++;
+            }
+            while(arr[r] == 1 && l<r){
+                r--;
+            }
+            if(l<r){
+                int temp = arr[l];
+                arr[l] = arr[r];
+                arr[r] = temp;
+                l++;
+                r--;
+            }
+        }
+    }
+}
