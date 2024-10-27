ad12bb0ba07c298eee1d3fbb041952036cafc377
Shubham Gupta <shubham272101@gmail.com>
2024-10-27 14:22:33 +0530

    add transposeMatrix and MatrixDiagonalSum problems

---
+++ MatrixDiagonalSum.java
@@ -0,0 +1,23 @@
+package DSA;
+class MatrixDiagonalSum {
+    public int diagonalSum(int[][] mat) {
+        int r = mat.length; // calculate length
+        int c = r; // calculate columns -> bcs its a square matrix
+        int ans = 0; // total sum
+
+        // primary
+        for(int i=0;i<r;i++){
+            ans += mat[i][i]; 
+        }
+
+        //secondary
+        for(int i=0;i<r;i++){
+            if(i==(c-i-1)){ 
+                ans+=0;
+            }else{
+                ans+=mat[i][c-i-1];
+            }        
+        }
+        return ans;
+    }
+}
---
+++ TransposeMatrix.java
@@ -0,0 +1,15 @@
+package DSA;
+
+public class TransposeMatrix {
+    public int[][] transpose(int[][] matrix) {
+        int r = matrix.length;
+        int c = matrix[0].length;
+        int temp[][] = new int[c][r];
+        for(int i=0;i<r;i++){
+            for(int j=0;j<c;j++){
+                temp[j][i] = matrix[i][j];
+            }
+        }
+        return temp;
+    }
+}
