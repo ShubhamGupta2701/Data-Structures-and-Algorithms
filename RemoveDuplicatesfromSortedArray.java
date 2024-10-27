c8e551001f5ccaa150154f69c21373d718b0c8ad
Shubham Gupta <shubham272101@gmail.com>
2024-10-27 21:31:50 +0530

    add Remove Duplicates From Sorted Array

---
+++ RemoveDuplicatesfromSortedArray.java
@@ -0,0 +1,14 @@
+public class RemoveDuplicatesfromSortedArray {
+    public int removeDuplicates(int[] nums) {
+        int n = nums.length;
+        int i=0,j=0,prev=-101;
+        for(i=0;i<n;i++){
+            if(nums[i] != prev){
+                prev = nums[i];
+                nums[j] = prev;
+                j++;
+            } 
+        }
+        return j;
+    }
+}
