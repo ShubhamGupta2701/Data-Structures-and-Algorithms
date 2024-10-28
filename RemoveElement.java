4743654d0f108b8e9f27610dd56b6f94f2ca47ea
Shubham Gupta <shubham272101@gmail.com>
2024-10-28 17:12:47 +0530

    add Remove Element Problem

---
+++ RemoveElement.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+
+class RemoveElement{
+    public int removeElement(int[] nums, int val) {
+        int n = nums.length;
+        int unique=0;
+        for(int i=0;i<n;i++){
+            if(nums[i] == val) nums[i] = 51;
+            else unique++;
+        }
+        Arrays.sort(nums);
+        return unique;
+    }
+}
