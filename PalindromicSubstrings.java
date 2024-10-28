1229732b9d75a3229c1656d8101067189398f29c
Shubham Gupta <shubham272101@gmail.com>
2024-10-28 17:15:41 +0530

    add Palindromic SubStrings Problem

---
+++ PalindromicSubstrings.java
@@ -0,0 +1,23 @@
+public class PalindromicSubstrings {
+    public boolean isPalindrome(String s){
+        int i=0;
+        int n = s.length()-1;
+        while(i<=n){
+            if(s.charAt(i)!=s.charAt(n)) return false;
+            i++;
+            n--;
+        }
+        return true;
+    }
+    public int countSubstrings(String s) {
+        int ans=0;
+        int n = s.length();
+        for(int i=0;i<n;i++){
+            for(int j=i;j<n;j++){
+                String str = s.substring(i,j+1);
+                if(isPalindrome(str)) ans++; 
+            }
+        }
+        return ans;
+    }
+}
