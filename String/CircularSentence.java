09c945e04df1508d48fb9205c884aaaa81125856
Shubham Gupta <shubham272101@gmail.com>
2024-11-02 20:09:31 +0530

    add circular Sentence problem

---
+++ String/CircularSentence.java
@@ -0,0 +1,14 @@
+package String;
+
+public class CircularSentence {
+    public boolean isCircularSentence(String sentence) {
+        int size = sentence.length();
+        for(int i=0; i<size-1; i++){
+            if(sentence.charAt(i) == ' '){
+                if(sentence.charAt(i-1) != sentence.charAt(i+1)) return false;
+            }
+        }
+        if(sentence.charAt(0) != sentence.charAt(size-1)) return false;
+        return true;
+    }
+}
